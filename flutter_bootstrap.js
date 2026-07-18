ea1c7b1bc483ffbdf49a44c34f1f44124a31f22c
oeria112232-art <oeria112232@gmail.com>
2026-07-19 00:46:38 +0300

    Rebuild: tumbleweed desert meme page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "633780224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3605560271" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
