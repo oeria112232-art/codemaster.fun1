ed9a7d5ac9b2f3d43adafe09bb82f08fc42f62a7
oeria112232-art <oeria112232@gmail.com>
2026-07-18 23:39:58 +0300

    Rebuild: stats mobile 2-per-row, +200 clients, smooth scroll, sample gallery overhaul

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3135930372" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "633780224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
