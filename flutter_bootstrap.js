8682100d6e1865d9d873fe6088539990b5ed23b9
oeria112232-art <oeria112232@gmail.com>
2026-07-18 03:33:36 +0300

    Deploy: ultra cosmic galaxy rewrite

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1103059727" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1194509716" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
