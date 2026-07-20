f9df4241effa81e76917d8d10f1b63c1e59c8d27
oeria112232-art <oeria112232@gmail.com>
2026-07-20 22:52:13 +0300

    Fix CSP: allow gstatic + cloudflareinsights

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1456571308" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1504738325" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
